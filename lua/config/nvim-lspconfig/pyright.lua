34eafae0c6a53f9eb11badd86f3da72ddedde195
Zhifan Chen <chenzhifan9@gmail.com>
2021-04-24 16:41:44 +0200

    Clean up

--- lua/config/nvim-lspconfig/pyright.lua
+++ lua/config/nvim-lspconfig/pyright.lua
@@ -1,19 +1,3 @@
-local util = require 'lspconfig/util'
 local pyright = require'lspconfig'.pyright
-local root_files = {
-    'setup.py',
-    'pyproject.toml',
-    'setup.cfg',
-    'requirements.txt',
-    '.git',
-    bufdir,
-}
 
-pyright.setup{
-    default_config = {
-        root_dir = function(filename)
-            return util.root_pattern(unpack(root_files))(filename) or
-                util.path.dirname(filename)
-        end;
-    };
-}
+pyright.setup{}
