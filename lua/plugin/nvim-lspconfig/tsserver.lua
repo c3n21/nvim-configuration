021ef3b017edf6a2fec2daf17b23964e3f69f435
Zhifan Chen <chenzhifan9@gmail.com>
2021-04-24 11:23:10 +0200

    Working tsserver

--- lua/plugin/nvim-lspconfig/tsserver.lua
+++ lua/plugin/nvim-lspconfig/tsserver.lua
@@ -1,4 +1,2 @@
-return function ()
-  local nvim_lsp = require('lspconfig')
-	nvim_lsp.tsserver.setup { on_attach = require('completion').on_attach }
-end
+local nvim_lsp = require('lspconfig')
+nvim_lsp.tsserver.setup{}
