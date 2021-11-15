1e74a26648866d3ca6aaf8ffe29aab1f8fd828b9
Zhifan Chen <me@zhifan.me>
2021-11-15 15:22:48 +0100

    modify(nvim-jdtls): effectively disabled nvim-jdtls config file

--- lua/config/nvim-jdtls/init.lua
+++ lua/config/nvim-jdtls/init.lua
@@ -1,12 +1,12 @@
-local coq = require "coq" -- add this
-local jdtls = require("jdtls")
-
-local config = coq.lsp_ensure_capabilities({
-        cmd = {
-            '/usr/bin/jdtls',
-            os.getenv('HOME') .. '/workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(),':p:h:t')
-        },
-        root_dir = jdtls.setup.find_root({'gradle.build', 'pom.xml', ".git"})
-    })
-
-jdtls.start_or_attach(config)
+--local coq = require "coq" -- add this
+--local jdtls = require("jdtls")
+--
+--local config = coq.lsp_ensure_capabilities({
+--        cmd = {
+--            '/usr/bin/jdtls',
+--            os.getenv('HOME') .. '/workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(),':p:h:t')
+--        },
+--        root_dir = jdtls.setup.find_root({'gradle.build', 'pom.xml', ".git"})
+--    })
+--
+--jdtls.start_or_attach(config)
