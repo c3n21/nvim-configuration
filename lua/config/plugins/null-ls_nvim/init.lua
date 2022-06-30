35bcb9a8131340738a05de0a9b554ab165b0945f
Zhifan <me@zhifan.me>
2022-06-30 07:28:54 +0200

    add: null-ls as default formatter

--- lua/config/plugins/null-ls_nvim/init.lua
+++ lua/config/plugins/null-ls_nvim/init.lua
@@ -4,7 +4,11 @@ local sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.prettier
 }
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
     cmd = { 'nvim' },
     debounce = 250,
@@ -17,7 +21,19 @@ null_ls.setup({
         level = 'warn',
         use_console = 'async',
     },
-    on_attach = nil,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format({bufnr = bufnr})
+                end,
+            })
+        end
+    end,
     on_init = nil,
     on_exit = nil,
     -- root_dir = u.root_pattern(".null-ls-root", "Makefile", ".git"),
