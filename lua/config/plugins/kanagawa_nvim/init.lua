4cdacff93a0e2f51651156686fd80dc4ca66ee5d
Zhifan <me@zhifan.me>
2022-07-29 16:12:51 +0200

    change(kanagawa): transparent background, dim windows

--- lua/config/plugins/kanagawa_nvim/init.lua
+++ lua/config/plugins/kanagawa_nvim/init.lua
@@ -9,8 +9,8 @@ require('kanagawa').setup({
     variablebuiltinStyle = { italic = true },
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
-    transparent = false, -- do not set background color
-    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+    transparent = true, -- do not set background color
+    dimInactive = true, -- dim inactive window `:h hl-NormalNC`
     globalStatus = false, -- adjust window separators highlight for laststatus=3
     colors = {},
     overrides = {},
