b8c029199b504a61566d0af288fdc43898db20fe
c3n21 <me@zhifan.me>
2022-09-02 22:11:06 +0200

    change(null-ls): revert to default

--- lua/config/plugins/null-ls_nvim/init.lua
+++ lua/config/plugins/null-ls_nvim/init.lua
@@ -1,37 +1,17 @@
-local h = require('null-ls.helpers')
 local null_ls = require('null-ls')
 
-local severities = {
-    Warning = vim.diagnostic.severity.WARN,
-    Error = vim.diagnostic.severity.ERROR,
-}
 -- register any number of sources simultaneously
 local sources = {
     null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.code_actions.gitsigns,
+
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.phpcsfixer,
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.formatting.tidy,
-    null_ls.builtins.diagnostics.tidy.with({
-        args = function(params)
-            local common_args = {
-                '-quiet',
-                '-errors',
-            }
-            if params.ft == 'xml' then
-                table.insert(common_args, 1, '-xml')
-            end
 
-            return common_args
-        end,
-        on_output = h.diagnostics.from_pattern(
-            [[line (%d+) column (%d+) %- (%a+): (.+)]],
-            { 'row', 'col', 'severity', 'message' },
-            { severities = severities }
-        ),
-    }),
+    null_ls.builtins.diagnostics.tidy,
     --[[ null_ls.builtins.diagnostics.phpcs, ]]
     --[[ null_ls.builtins.diagnostics.phpmd, ]]
     --[[ null_ls.builtins.diagnostics.phpstan, ]]
