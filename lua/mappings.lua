8c83004c52d709c02036b70044b69742a9f6e88b
Zhifan <me@zhifan.me>
2022-10-24 09:08:59 +0200

    move lsp maps in mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,6 @@
 local opts = { noremap = true, silent = true }
+
+-- Generic
 vim.keymap.set({ 'x' }, '<M-K>', ':m-2 <CR>gv=gv', opts)
 vim.keymap.set({ 'n' }, '<M-K>', ':<C-u>m-2<CR>==', opts)
 vim.keymap.set({ 'n' }, '<M-J>', ':<C-u>m-2<CR>==', opts)
@@ -13,6 +15,21 @@ vim.keymap.set({ 'n' }, ']q', ':cnext <CR>', opts)
 vim.keymap.set({ 'n' }, '[q', ':cprevious <CR>', opts)
 vim.keymap.set({ 'n' }, '<leader>tn', ':tabe %<CR>', opts)
 vim.keymap.set({ 'n' }, '<leader><leader>i', ':luafile ' .. os.getenv('MYVIMRC') .. '<CR>', opts)
+
+-- LSP
+vim.keymap.set({ 'n' }, 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set({ 'n' }, 'gD', vim.lsp.buf.type_definition, opts)
+vim.keymap.set({ 'n' }, 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set({ 'n' }, 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set({ 'n' }, 'H', vim.lsp.buf.signature_help, opts)
+vim.keymap.set({ 'n' }, '<leader>D', vim.lsp.buf.type_definition, opts)
+vim.keymap.set({ 'n' }, '<leader>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+vim.keymap.set({ 'n' }, '<leader>d', vim.diagnostic.open_float, opts)
+vim.keymap.set({ 'n' }, '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set({ 'n' }, ']d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set({ 'n' }, '<leader>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set({ 'n' }, '<leader><leader>f', vim.lsp.buf.format, opts)
 
 for _, breakpoint in ipairs({ ',', '.', '[', ']', '!', '?' }) do
   vim.keymap.set({ 'i' }, breakpoint, breakpoint .. '<c-g>u', opts)
