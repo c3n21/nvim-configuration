962db794b1cbbd0fb4e233baf2a7d9e4fedd4e81
Zhifan <me@zhifan.me>
2022-12-09 14:04:44 +0100

    feat(lsp): go to declaration mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,7 +17,8 @@ vim.keymap.set({ 'n' }, '<leader>tn', ':tabe %<CR>', opts)
 vim.keymap.set({ 'n' }, '<leader><leader>i', ':luafile ' .. os.getenv('MYVIMRC') .. '<CR>', opts)
 
 -- LSP
-vim.keymap.set({ 'n' }, 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set({ 'n' }, '<leader>D', vim.lsp.buf.definition, opts)
+vim.keymap.set({ 'n' }, 'gd', vim.lsp.buf.declaration, opts)
 vim.keymap.set({ 'n' }, 'gD', vim.lsp.buf.type_definition, opts)
 vim.keymap.set({ 'n' }, 'K', vim.lsp.buf.hover, opts)
 vim.keymap.set({ 'n' }, 'gi', vim.lsp.buf.implementation, opts)
