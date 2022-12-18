9230fc3dc6cd8a24bd51859fd17e9af0a203aa37
c3n21 <me@zhifan.me>
2022-12-18 19:48:06 +0100

    feat(null-ls): erlfmt

--- lua/config/plugins/null-ls_nvim/init.lua
+++ lua/config/plugins/null-ls_nvim/init.lua
@@ -35,6 +35,7 @@ local sources = {
     --[[ }), ]]
     --[[ null_ls.builtins.diagnostics.eslint_d, ]]
     null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.erlfmt,
 }
 
 null_ls.setup({
