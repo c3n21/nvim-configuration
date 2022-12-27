cd35b76d6fe829f471da3810db0787c030f835a7
Zhifan <me@zhifan.me>
2022-12-27 10:22:14 +0100

    fix(auto-session): restoring session even in gitrebase

--- lua/plugins/auto-session/init.lua
+++ lua/plugins/auto-session/init.lua
@@ -1,17 +1,27 @@
 return {
     'rmagatti/auto-session',
     config = function()
-        vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal'
+        vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
         require('auto-session').setup({
-            bypass_session_save_file_types = { 'gitrebase' }, -- boolean: Bypass auto save when only buffer open is one of these file types
-            cwd_change_handling = { -- table: Config for handling the DirChangePre and DirChanged autocmds, can be set to nil to disable altogether
-                restore_upcoming_session = false, -- boolean: restore session for upcoming cwd on cwd change
-                pre_cwd_changed_hook = nil, -- function: This is called after auto_session code runs for the `DirChangedPre` autocmd
-                post_cwd_changed_hook = nil, -- function: This is called after auto_session code runs for the `DirChanged` autocmd
-            },
-            auto_session_use_git_branch = true,
-            auto_restore_enabled = false,
+            log_level = 'info',
+            auto_session_enable_last_session = false,
+            auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
+            auto_session_enabled = true,
             auto_save_enabled = true,
+            auto_restore_enabled = false,
+            auto_session_suppress_dirs = nil,
+            auto_session_use_git_branch = true,
+            -- the configs below are lua only
+            bypass_session_save_file_types = {
+                'dashboard',
+                'NvimTree',
+                'packer',
+                'startify',
+                'fugitiveblame',
+                'gitrebase',
+                'gitcommit',
+                --[[ 'toggleterm', ]]
+            },
         })
     end,
 }
