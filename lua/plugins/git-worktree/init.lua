fd6c18e3b2f62209fa0d32ac96e6255f2cc87949
Zhifan <me@zhifan.me>
2023-01-04 14:48:24 +0100

    feat(git-worktree): set new keymaps

--- lua/plugins/git-worktree/init.lua
+++ lua/plugins/git-worktree/init.lua
@@ -1,5 +1,9 @@
 return {
     'ThePrimeagen/git-worktree.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
     config = function()
         require('git-worktree').setup({
             update_on_change = true, -- default: true,
@@ -9,5 +13,19 @@ return {
         })
 
         require('telescope').load_extension('git_worktree')
+
+        vim.keymap.set(
+            'n',
+            '<leader>gw',
+            require('telescope').extensions.git_worktree.git_worktrees,
+            { noremap = true, silent = true }
+        )
+
+        vim.keymap.set(
+            'n',
+            '<leader>gc',
+            require('telescope').extensions.git_worktree.create_git_worktree,
+            { noremap = true, silent = true }
+        )
     end,
 }
