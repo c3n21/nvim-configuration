0e8137ae64d0d35e0eafa320dd0ce3dc5f4aca25
c3n21 <me@zhifan.me>
2023-01-23 08:36:04 +0100

    fix(neogit): add deps

--- lua/plugins/neogit/init.lua
+++ lua/plugins/neogit/init.lua
@@ -1,5 +1,9 @@
 return {
     'TimUntersberger/neogit',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'sindrets/diffview.nvim',
+    },
     config = function()
         require('neogit').setup({
             disable_signs = false,
