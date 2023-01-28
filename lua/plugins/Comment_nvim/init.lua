f691f7f8416a33d15e7161e29fdfc7595568cd7a
Zhifan <me@zhifan.me>
2023-01-28 08:42:54 +0100

    fix(Comment): remove pre_hook

--- lua/plugins/Comment_nvim/init.lua
+++ lua/plugins/Comment_nvim/init.lua
@@ -79,21 +79,7 @@ return {
 
             ---Pre-hook, called before commenting the line
             ---@type fun(ctx: Ctx):string
-            pre_hook = function(ctx)
-                local U = require('Comment.utils')
-
-                local location = nil
-                if ctx.ctype == U.ctype.block then
-                    location = require('ts_context_commentstring.utils').get_cursor_location()
-                elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-                    location = require('ts_context_commentstring.utils').get_visual_start_location()
-                end
-
-                return require('ts_context_commentstring.internal').calculate_commentstring({
-                    key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-                    location = location,
-                })
-            end,
+            pre_hook = nil,
 
             ---Post-hook, called after commenting is done
             ---@type fun(ctx: Ctx)
