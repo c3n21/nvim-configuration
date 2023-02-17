6662f4229f46cce59ffa1f8c2c6624b18d1cd59f
Zhifan <me@zhifan.me>
2023-02-17 16:02:04 +0100

    feat(project_nvim): telescope extension

--- lua/plugins/project_nvim/init.lua
+++ lua/plugins/project_nvim/init.lua
@@ -1,10 +1,13 @@
 return {
     'ahmedkhalf/project.nvim',
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+    },
     config = function()
         require('project_nvim').setup({
             -- Manual mode doesn't automatically change your root directory, so you have
             -- the option to manually do so using `:ProjectRoot` command.
-            manual_mode = false,
+            manual_mode = true,
 
             -- Methods of detecting the root directory. **"lsp"** uses the native neovim
             -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
@@ -29,11 +32,16 @@ return {
 
             -- When set to false, you will get a message when project.nvim changes your
             -- directory.
-            silent_chdir = true,
+            silent_chdir = false,
 
             -- Path where project.nvim will store the project history for use in
             -- telescope
             datapath = vim.fn.stdpath('data'),
         })
+        require('telescope').load_extension('projects')
+
+        vim.keymap.set('n', '<leader>p', function()
+            require('telescope').extensions.projects.projects({})
+        end, { noremap = true, silent = true })
     end,
 }
