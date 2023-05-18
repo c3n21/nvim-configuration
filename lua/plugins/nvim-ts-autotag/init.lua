0b93167b06dc4897d7ab9690445ee82eef96f011
Zhifan <me@zhifan.me>
2023-05-18 03:37:00 +0200

    chore(nvim-ts-autotag): remove useless code

--- lua/plugins/nvim-ts-autotag/init.lua
+++ lua/plugins/nvim-ts-autotag/init.lua
@@ -1,30 +1,6 @@
 return {
     'windwp/nvim-ts-autotag',
     config = function()
-        local filetypes = {
-            'html',
-            'javascript',
-            'typescript',
-            'javascriptreact',
-            'typescriptreact',
-            'jsx',
-            'tsx',
-            'svelte',
-            'vue',
-            'tsx',
-            'jsx',
-            'rescript',
-            'xml',
-            'php',
-            'markdown',
-            'glimmer',
-            'handlebars',
-            'hbs',
-            'phtml',
-        }
-
-        require('nvim-ts-autotag').setup({
-            filetypes = filetypes,
-        })
+        require('nvim-ts-autotag').setup()
     end,
 }
