0f41963472c73a36b0218f075ff62a2926801c33
Zhifan <me@zhifan.me>
2023-08-16 18:13:36 +0200

    feat(lsp): erlangls

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -8,6 +8,7 @@ return {
     dockerls = {},
     svelte = {},
     html = {},
+    erlangls = {},
     intelephense = {
         -- root_dir = lspconfig.util.root_pattern('composer.json'),
         root_dir = lspconfig.util.root_pattern('.git'),
