6d38b8be6b0ffec7274c1945614b8940c2427e43
Zhifan <me@zhifan.me>
2023-09-09 22:54:01 +0200

    feat(plugins/typescript): configure typescript-tools

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,31 +1,34 @@
 local map_opts = { noremap = true, silent = true }
 
 local mappings_enum = {
+    ['BufferClose'] = '<leader>bq',
+    ['CClose'] = '<leader>qq',
+    ['CNext'] = ']q',
+    ['COpen'] = '<leader>qo',
+    ['CPrev'] = '[q',
+    ['CodeActions'] = '<leader>ca',
+    ['DiagnosticErrorNext'] = '<leader>]e',
+    ['DiagnosticErrorPrev'] = '<leader>[e',
+    ['DiagnosticInfoNext'] = '<leader>]i',
+    ['DiagnosticInfoPrev'] = '<leader>[i',
+    ['DiagnosticNext'] = '<leader>]d',
+    ['DiagnosticPrev'] = '<leader>[d',
+    ['DiagnosticWarningNext'] = '<leader>]w',
+    ['DiagnosticWarningPrev'] = '<leader>[w',
     ['FindFiles'] = '<leader>ff',
+    ['Format'] = '==',
+    ['GoToDefinitionTab'] = '<C-w><C-]>',
+    ['LClose'] = '<leader>lq',
+    ['LNext'] = ']l',
+    ['LOpen'] = '<leader>lo',
+    ['LPrev'] = '[l',
+    ['LspReferences'] = 'gr',
+    ['OpenDiagnosticLoclist'] = '<leader>q',
+    ['OpenFloatDiagnostic'] = '<leader><leader>d',
+    ['Rename'] = '<leader>rn',
     ['SourceInit'] = '<leader><leader>i',
     ['TabNext'] = '<leader>tn',
-    ['CPrev'] = '[q',
-    ['CNext'] = ']q',
-    ['CClose'] = '<leader>qq',
-    ['COpen'] = '<leader>qo',
-    ['LPrev'] = '[l',
-    ['LNext'] = ']l',
-    ['LClose'] = '<leader>lq',
-    ['LOpen'] = '<leader>lo',
-    ['BufferClose'] = '<leader>bq',
-    ['CodeActions'] = '<leader>ca',
-    ['OpenFloatDiagnostic'] = '<leader><leader>d',
-    ['PrevDiagnosticInfo'] = '[d',
-    ['NextDiagnosticInfo'] = ']d',
-    ['PrevDiagnosticWarning'] = '[w',
-    ['NextDiagnosticWarning'] = ']w',
-    ['PrevDiagnosticError'] = '[e',
-    ['NextDiagnosticError'] = ']e',
-    ['OpenDiagnosticLoclist'] = '<leader>q',
-    ['GoToDefinitionTab'] = '<C-w><C-]>',
-    ['LspReferences'] = 'gr',
     ['ToggleInlayHints'] = 'gK',
-    ['Format'] = '==',
 }
 
 -- Generic
@@ -65,71 +68,91 @@ end
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', mappings_enum['DiagnosticPrev'], vim.diagnostic.goto_prev)
+vim.keymap.set('n', mappings_enum['DiagnosticNext'], vim.diagnostic.goto_next)
+vim.keymap.set({ 'n', 'v' }, mappings_enum['CodeActions'], vim.lsp.buf.code_action, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['OpenFloatDiagnostic'], vim.diagnostic.open_float, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['DiagnosticInfoPrev'], function()
+    vim.diagnostic.goto_prev({ wrap = false, severity = { max = vim.diagnostic.severity.INFO } })
+end, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['DiagnosticInfoNext'], function()
+    vim.diagnostic.goto_next({ wrap = false, severity = { max = vim.diagnostic.severity.INFO } })
+end, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['DiagnosticWarningPrev'], function()
+    vim.diagnostic.goto_prev({ wrap = false, severity = vim.diagnostic.severity.WARN })
+end, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['DiagnosticWarningNext'], function()
+    vim.diagnostic.goto_next({ wrap = false, severity = vim.diagnostic.severity.WARN })
+end, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['DiagnosticErrorPrev'], function()
+    vim.diagnostic.goto_prev({ wrap = false, severity = vim.diagnostic.severity.ERROR })
+end, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['DiagnosticErrorNext'], function()
+    vim.diagnostic.goto_next({ wrap = false, severity = vim.diagnostic.severity.ERROR })
+end, map_opts)
+vim.keymap.set({ 'n' }, mappings_enum['OpenDiagnosticLoclist'], vim.diagnostic.setloclist, map_opts)
+vim.keymap.set('n', mappings_enum['Rename'], vim.lsp.buf.rename, map_opts)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+--     callback = function(ev)
+--         -- Enable completion triggered by <c-x><c-o>
+--         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        -- vim.keymap.set('n', '<space>wl', function()
-        --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, opts)
-        -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        -- vim.keymap.set('n', '<space>f', function()
-        --   vim.lsp.buf.format { async = true }
-        -- end, opts)
+--         -- Buffer local mappings.
+--         -- See `:help vim.lsp.*` for documentation on any of the below functions
+--         local opts = { buffer = ev.buf }
+--         -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+--         -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--         -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+--         -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--         -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--         -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+--         -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--         -- vim.keymap.set('n', '<space>wl', function()
+--         --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--         -- end, opts)
+--         -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+--         -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+--         -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+--         -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--         -- vim.keymap.set('n', '<space>f', function()
+--         --   vim.lsp.buf.format { async = true }
+--         -- end, opts)
 
-        local builtin = require('telescope.builtin')
+--         local builtin = require('telescope.builtin')
 
-        -- vim.keymap.set({ 'n' }, 'gr', builtin.lsp_references, map_opts)
-        vim.keymap.set({ 'n' }, '<leader>ldd', builtin.diagnostics, opts)
-        --TODO: use these again
-        --[[ vim.keymap.set({ 'n' }, '<leader>ld', builtin.lsp_document_symbols, opts) ]]
-        --[[ vim.keymap.set({ 'n' }, '<leader>lw', builtin.lsp_workspace_symbols, opts) ]]
+--         -- vim.keymap.set({ 'n' }, 'gr', builtin.lsp_references, map_opts)
+--         vim.keymap.set({ 'n' }, '<leader>ldd', builtin.diagnostics, opts)
+--         --TODO: use these again
+--         --[[ vim.keymap.set({ 'n' }, '<leader>ld', builtin.lsp_document_symbols, opts) ]]
+--         --[[ vim.keymap.set({ 'n' }, '<leader>lw', builtin.lsp_workspace_symbols, opts) ]]
 
-        -- LSP
-        vim.keymap.set({ 'n' }, 'gD', builtin.lsp_type_definitions, opts)
-        -- Check hover plugin
-        vim.keymap.set({ 'n' }, 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set({ 'n' }, 'gi', builtin.lsp_implementations, opts)
-        vim.keymap.set({ 'n' }, 'gd', vim.lsp.buf.declaration, opts)
-        vim.keymap.set(
-            { 'n' },
-            mappings_enum['GoToDefinitionTab'],
-            '<cmd>tab split | lua vim.lsp.buf.definition()<CR> ',
-            opts
-        )
-        vim.keymap.set({ 'n' }, 'H', vim.lsp.buf.signature_help, opts)
-        --[[ vim.keymap.set({ 'n' }, '<leader>rn', vim.lsp.buf.rename, opts) ]]
-        vim.keymap.set('n', '<leader>rn', function()
-            return ':IncRename ' .. vim.fn.expand('<cword>')
-        end, { expr = true })
-        vim.keymap.set({ 'n' }, '<leader>lw', vim.lsp.buf.workspace_symbol, opts)
-        vim.keymap.set({ 'n' }, '<leader>ld', vim.lsp.buf.document_symbol, opts)
-        vim.keymap.set({ 'n' }, mappings_enum['ToggleInlayHints'], function()
-            vim.lsp.inlay_hint(0, nil)
-        end, opts)
-    end,
-})
+--         -- LSP
+--         vim.keymap.set({ 'n' }, 'gD', builtin.lsp_type_definitions, opts)
+--         -- Check hover plugin
+--         vim.keymap.set({ 'n' }, 'K', vim.lsp.buf.hover, opts)
+--         vim.keymap.set({ 'n' }, 'gi', builtin.lsp_implementations, opts)
+--         vim.keymap.set({ 'n' }, 'gd', vim.lsp.buf.declaration, opts)
+--         vim.keymap.set(
+--             { 'n' },
+--             mappings_enum['GoToDefinitionTab'],
+--             '<cmd>tab split | lua vim.lsp.buf.definition()<CR> ',
+--             opts
+--         )
+--         vim.keymap.set({ 'n' }, 'H', vim.lsp.buf.signature_help, opts)
+--         --[[ vim.keymap.set({ 'n' }, '<leader>rn', vim.lsp.buf.rename, opts) ]]
+--         vim.keymap.set('n', '<leader>rn', function()
+--             return ':IncRename ' .. vim.fn.expand('<cword>')
+--         end, { expr = true })
+--         vim.keymap.set({ 'n' }, '<leader>lw', vim.lsp.buf.workspace_symbol, opts)
+--         vim.keymap.set({ 'n' }, '<leader>ld', vim.lsp.buf.document_symbol, opts)
+--         vim.keymap.set({ 'n' }, mappings_enum['ToggleInlayHints'], function()
+--             vim.lsp.inlay_hint(0, nil)
+--         end, opts)
+--     end,
+-- })
 
 return mappings_enum
--- lua/plugins/typescript/init.lua
+++ lua/plugins/typescript/init.lua
@@ -1,26 +1,53 @@
 local map_opts = { noremap = true, silent = true }
+
 return {
-    'jose-elias-alvarez/typescript.nvim',
+    'pmizio/typescript-tools.nvim',
     dependencies = {
-        'folke/neoconf.nvim',
-        'folke/noice.nvim',
+        'lsp',
     },
     ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
-    enabled = false,
     config = function()
-        require('typescript').setup({
-            disable_commands = false, -- prevent the plugin from creating Vim commands
-            debug = false, -- enable debug logging for commands
-            go_to_source_definition = {
-                fallback = true, -- fall back to standard LSP definition on failure
-            },
+        local mason_registry = require('mason-registry')
+        local tsserver_path = mason_registry.get_package('typescript-language-server'):get_install_path()
+        require('typescript-tools').setup({
+            on_attach = function(client, bufnr)
+                require('plugins.lsp.on_attach').lsp_attach(client, bufnr)
+                vim.keymap.set({ 'n', 'i' }, '<leader>gsd', '<cmd>TSToolsGoToSourceDefinition<CR>', map_opts)
+            end,
+            settings = {
 
-            server = { -- pass options to lspconfig's setup method
-                on_attach = function(client, bufnr)
-                    require('plugins.lsp.on_attach').lsp_attach(client, bufnr)
-                    vim.keymap.set('n', '<leader>rf', '<cmd>TypescriptRenameFile<CR>', map_opts)
-                    vim.keymap.set({ 'n', 'i' }, '<C>]', '<cmd>TypescriptGoToSourceDefinition<CR>', map_opts)
-                end,
+                -- spawn additional tsserver instance to calculate diagnostics on it
+                separate_diagnostic_server = true,
+                -- "change"|"insert_leave" determine when the client asks the server about diagnostic
+                publish_diagnostic_on = 'insert_leave',
+                -- array of strings("fix_all"|"add_missing_imports"|"remove_unused")
+                -- specify commands exposed as code_actions
+                expose_as_code_action = {
+                    'fix_all',
+                    'add_missing_imports',
+                    'remove_unused',
+                },
+                -- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
+                -- not exists then standard path resolution strategy is applied
+                tsserver_path = tsserver_path .. '/node_modules/typescript/lib/tsserver.js',
+                -- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
+                -- (see 💅 `styled-components` support section)
+                tsserver_plugins = {},
+                -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
+                -- memory limit in megabytes or "auto"(basically no limit)
+                tsserver_max_memory = 'auto',
+                -- described below
+                tsserver_format_options = {},
+                tsserver_file_preferences = {
+                    includeInlayParameterNameHints = 'all',
+                    includeCompletionsForModuleExports = true,
+                    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                    includeInlayFunctionParameterTypeHints = true,
+                    includeInlayVariableTypeHints = true,
+                    includeInlayPropertyDeclarationTypeHints = true,
+                    includeInlayFunctionLikeReturnTypeHints = true,
+                    includeInlayEnumMemberValueHints = true,
+                },
             },
         })
     end,
