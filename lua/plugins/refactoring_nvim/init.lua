f7bd548e6ef6432483b36490cd64d550f2427dbc
Zhifan <me@zhifan.me>
2023-10-06 21:54:48 +0200

    fix(plugins/refactoring_nvim): add deps

--- lua/plugins/refactoring_nvim/init.lua
+++ lua/plugins/refactoring_nvim/init.lua
@@ -1,5 +1,9 @@
 return {
     'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+    },
     config = function()
         require('refactoring').setup({
             prompt_func_return_type = {
