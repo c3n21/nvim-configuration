4e00737a24703b141f72461b16a2f53a78087317
c3n21 <me@zhifan.me>
2023-12-05 08:59:01 +0100

    Bugfix/inlay hint (#7)
    * fix(plugins/lsp/on_attach): use new API for toggling inlay_hint
    
    * feat(plugins/lsp/on_attach): don't enable inlay_hint on attach

--- lua/plugins/lsp/on_attach.lua
+++ lua/plugins/lsp/on_attach.lua
@@ -43,10 +43,6 @@ local function lsp_attach(client, bufnr)
         vim.keymap.set({ 'n' }, mappings_enum['SignatureHelp'], vim.lsp.buf.signature_help, map_opts)
     end
 
-    if client.server_capabilities.inlayHintProvider then
-        vim.lsp.inlay_hint.enable(0, true)
-    end
-
     vim.keymap.set('n', mappings_enum['DiagnosticPrev'], vim.diagnostic.goto_prev)
     vim.keymap.set('n', mappings_enum['DiagnosticNext'], vim.diagnostic.goto_next)
     vim.keymap.set({ 'n', 'v' }, mappings_enum['CodeActions'], vim.lsp.buf.code_action, map_opts)
@@ -74,7 +70,7 @@ local function lsp_attach(client, bufnr)
         return ':IncRename ' .. vim.fn.expand('<cword>')
     end, { expr = true })
     vim.keymap.set({ 'n' }, mappings_enum['ToggleInlayHints'], function()
-        vim.lsp.inlay_hint(0, nil)
+        vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0))
     end, map_opts)
 end
 
