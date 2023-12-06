48d115b8e459a7d8d3eed6099d9a151ddd19f268
Zhifan <me@zhifan.me>
2023-12-06 09:19:45 +0100

    feat(plugins/nvim-treesitter-refactor): disable navigation

--- lua/plugins/nvim-treesitter-refactor/init.lua
+++ lua/plugins/nvim-treesitter-refactor/init.lua
@@ -6,7 +6,7 @@ return {
                 highlight_definitions = { enable = true },
                 highlight_current_scope = { enable = true },
                 navigation = {
-                    enable = true,
+                    enable = false,
                     keymaps = {
                         goto_definition = 'gnd',
                         list_definitions = 'gnD',
