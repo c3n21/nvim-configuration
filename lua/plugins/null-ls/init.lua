95ca88dfc306a2e1ba28fe1059ff6ce59cfb1e70
c3n21 <chenzhifan9@gmail.com>
2023-12-13 09:46:02 +0100

    feat(null-ls): remove eslint_d code actions

--- lua/plugins/null-ls/init.lua
+++ lua/plugins/null-ls/init.lua
@@ -14,7 +14,6 @@ return {
             -- code actions
             null_ls.builtins.code_actions.refactoring,
             null_ls.builtins.code_actions.gitsigns,
-            null_ls.builtins.code_actions.eslint_d,
 
             -- diagnostics
             null_ls.builtins.diagnostics.tidy,
