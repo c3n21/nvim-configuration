62244cb53306ea66732b41be26e7df90023f525a
c3n21 <chenzhifan9@gmail.com>
2024-01-03 13:29:32 +0100

    feat(conform): organize imports for ts

--- lua/plugins/formatting/init.lua
+++ lua/plugins/formatting/init.lua
@@ -1,6 +1,6 @@
 --- @type LazyPlugin
 return {
-    'https://github.com/stevearc/conform.nvim',
+    'stevearc/conform.nvim',
     opts = {
         formatters_by_ft = {
             lua = { 'stylua' },
@@ -13,11 +13,31 @@ return {
             typescriptreact = { { 'prettierd', 'prettier' } },
             nix = { 'nixpkgs-fmt' },
         },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
+        format_on_save = function(bufnr)
+            local ts = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' }
+            -- Disable autoformat on certain filetypes
+            local ignore_filetypes = {}
+
+            if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                return
+            end
+
+            if vim.tbl_contains(ts, vim.bo[bufnr].filetype) then
+                vim.cmd('TSToolsOrganizeImports sync')
+                vim.cmd('TSToolsAddMissingImports sync')
+            end
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if bufname:match('/node_modules/') then
+                return
+            end
+            -- ...additional logic...
+            return { timeout_ms = 500, lsp_fallback = true }
+        end,
         -- Set the log level. Use `:ConformInfo` to see the location of the log file.
         log_level = vim.log.levels.ERROR,
         -- Conform will notify you when a formatter errors
