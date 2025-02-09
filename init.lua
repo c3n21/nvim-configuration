0a8c61595ba48dc73c118c7e3b37e5e0344e99e6
c3n21 <chenzhifan9@gmail.com>
2025-02-09 18:30:03 +0100

    feat: highlight on yank

--- init.lua
+++ init.lua
@@ -5,4 +5,15 @@ if not success then
     vim.notify(fmt('Error loading settings: %s', vim.inspect(success)), vim.log.levels.WARN)
 end
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
+
 require('mappings')
