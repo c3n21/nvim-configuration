04dded54db23c3847b84931ca707d1aa1906b756
c3n21 <chenzhifan9@gmail.com>
2025-02-15 10:47:41 +0100

    feat: copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -52,6 +52,10 @@ require('blink.cmp').setup({
         },
     },
     appearance = {
+        kind_icons = {
+            Copilot = '',
+        },
+
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
         -- Useful for when your theme doesn't support blink.cmp
         -- Will be removed in a future release
@@ -64,6 +68,14 @@ require('blink.cmp').setup({
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'copilot', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+            copilot = {
+                name = 'copilot',
+                module = 'blink-copilot',
+                score_offset = 100,
+                async = true,
+            },
+        },
     },
 })
