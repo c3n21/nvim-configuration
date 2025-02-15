e7a2bb66c08c4626829fc7418275b3940b9b3ba5
c3n21 <chenzhifan9@gmail.com>
2025-02-15 20:29:38 +0100

    switched to vtsls

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -62,4 +62,18 @@ return {
     eslint = {},
     tailwindcss = {},
     jdtls = {},
+    vtsls = {
+        settings = {
+            typescript = {
+                inlayHints = {
+                    parameterNames = { enabled = 'literals' },
+                    parameterTypes = { enabled = true },
+                    variableTypes = { enabled = true },
+                    propertyDeclarationTypes = { enabled = true },
+                    functionLikeReturnTypes = { enabled = true },
+                    enumMemberValues = { enabled = true },
+                },
+            },
+        },
+    },
 }
