cb107490122d0da5d8141d3fdbd9ac1c3c5d21b5
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-04-22 10:52:53 +0200

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NeoVim configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      neovim-nightly,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages."${system}".default = neovim-nightly.packages."${system}".default;
+    };
+}
