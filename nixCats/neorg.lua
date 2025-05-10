3265569633a8154b3ae2faf64b5c19cdfff6ff29
c3n21 <nonno.felice69uwu@gmail.com>
2025-05-10 20:32:05 +0200

    fix: neorg (#24)

--- nixCats/neorg.lua
+++ nixCats/neorg.lua
@@ -5,11 +5,7 @@ neorg.setup({
         ['core.esupports.indent'] = {},
         ['core.defaults'] = {}, -- Load all the defaults
         ['core.completion'] = {
-            config = {
-                -- Note that this table is optional and doesn't need to be provided
-                -- Configuration here
-                engine = 'nvim-cmp',
-            },
+            config = { engine = { module_name = 'external.lsp-completion' } },
         },
         ['core.concealer'] = {
             -- config = {
@@ -17,6 +13,34 @@ neorg.setup({
             --     -- markup_preset = "dimmed",
             -- },
         }, -- Allows the use of icons
+        ['external.interim-ls'] = {
+            config = {
+                -- default config shown
+                completion_provider = {
+                    -- Enable or disable the completion provider
+                    enable = true,
+
+                    -- Show file contents as documentation when you complete a file name
+                    documentation = true,
+
+                    -- Try to complete categories provided by Neorg Query. Requires `benlubas/neorg-query`
+                    categories = false,
+
+                    -- suggest heading completions from the given file for `{@x|}` where `|` is your cursor
+                    -- and `x` is an alphanumeric character. `{@name}` expands to `[name]{:$/people:# name}`
+                    people = {
+                        enable = false,
+
+                        -- path to the file you're like to use with the `{@x` syntax, relative to the
+                        -- workspace root, without the `.norg` at the end.
+                        -- ie. `folder/people` results in searching `$/folder/people.norg` for headings.
+                        -- Note that this will change with your workspace, so it fails silently if the file
+                        -- doesn't exist
+                        path = 'people',
+                    },
+                },
+            },
+        },
         -- ["core.keybinds"] = {
         --     config = {
         --         default_keybinds = true,
@@ -72,8 +96,4 @@ neorg.setup({
     logger = {
         level = 'warn',
     },
-})
-
-neorg.modules.load_module('core.completion', nil, {
-    engine = 'nvim-cmp',
 })
