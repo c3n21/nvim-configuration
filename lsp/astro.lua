966f0c6b0aa8a42961635c25631b0656c5e892a7
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-06-11 10:18:28 +0200

    feat(lsp): astro

---
+++ lsp/astro.lua
@@ -0,0 +1,6 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
+
+return {
+    capabilities = capabilities,
+}
