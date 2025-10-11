219ebe93e922a96880acb6a99d19bb7d2917983d
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-10-11 16:12:54 +0200

    feat(blink-cmp): add fuzzy option

--- lua/configs/blink.lua
+++ lua/configs/blink.lua
@@ -13,6 +13,89 @@ local lua_source = vim.list_extend({
 }, default_sources)
 
 require('blink.cmp').setup({
+    fuzzy = {
+        -- Controls which implementation to use for the fuzzy matcher.
+        --
+        -- 'prefer_rust_with_warning' (Recommended) If available, use the Rust implementation, automatically downloading prebuilt binaries on supported systems. Fallback to the Lua implementation when not available, emitting a warning message.
+        -- 'prefer_rust' If available, use the Rust implementation, automatically downloading prebuilt binaries on supported systems. Fallback to the Lua implementation when not available.
+        -- 'rust' Always use the Rust implementation, automatically downloading prebuilt binaries on supported systems. Error if not available.
+        -- 'lua' Always use the Lua implementation, doesn't download any prebuilt binaries
+        --
+        -- See the prebuilt_binaries section for controlling the download behavior
+        implementation = 'prefer_rust_with_warning',
+
+        -- Allows for a number of typos relative to the length of the query
+        -- Set this to 0 to match the behavior of fzf
+        -- Note, this does not apply when using the Lua implementation.
+        max_typos = function(keyword)
+            return math.floor(#keyword / 4)
+        end,
+
+        -- Frecency tracks the most recently/frequently used items and boosts the score of the item
+        -- Note, this does not apply when using the Lua implementation.
+        frecency = {
+            -- Whether to enable the frecency feature
+            enabled = true,
+            -- Location of the frecency database
+            path = vim.fn.stdpath('state') .. '/blink/cmp/frecency.dat',
+            -- UNSAFE!! When enabled, disables the lock and fsync when writing to the frecency database.
+            -- This should only be used on unsupported platforms (i.e. alpine termux)
+            unsafe_no_lock = false,
+        },
+
+        -- Proximity bonus boosts the score of items matching nearby words
+        -- Note, this does not apply when using the Lua implementation.
+        use_proximity = true,
+
+        -- Controls which sorts to use and in which order, falling back to the next sort if the first one returns nil
+        -- You may pass a function instead of a string to customize the sorting
+        sorts = {
+            -- (optionally) always prioritize exact matches
+            -- 'exact',
+
+            -- pass a function for custom behavior
+            -- function(item_a, item_b)
+            --   return item_a.score > item_b.score
+            -- end,
+
+            'score',
+            'sort_text',
+        },
+
+        prebuilt_binaries = {
+            -- Whether or not to automatically download a prebuilt binary from github. If this is set to `false`,
+            -- you will need to manually build the fuzzy binary dependencies by running `cargo build --release`
+            -- Disabled by default when `fuzzy.implementation = 'lua'`
+            download = false,
+
+            -- Ignores mismatched version between the built binary and the current git sha, when building locally
+            ignore_version_mismatch = false,
+
+            -- When downloading a prebuilt binary, force the downloader to resolve this version. If this is unset
+            -- then the downloader will attempt to infer the version from the checked out git tag (if any).
+            --
+            -- Beware that if the fuzzy matcher changes while tracking main then this may result in blink breaking.
+            force_version = nil,
+
+            -- When downloading a prebuilt binary, force the downloader to use this system triple. If this is unset
+            -- then the downloader will attempt to infer the system triple from `jit.os` and `jit.arch`.
+            -- Check the latest release for all available system triples
+            --
+            -- Beware that if the fuzzy matcher changes while tracking main then this may result in blink breaking.
+            force_system_triple = nil,
+
+            -- Extra arguments that will be passed to curl like { 'curl', ..extra_curl_args, ..built_in_args }
+            extra_curl_args = {},
+
+            proxy = {
+                -- When downloading a prebuilt binary, use the HTTPS_PROXY environment variable
+                from_env = true,
+
+                -- When downloading a prebuilt binary, use this proxy URL. This will ignore the HTTPS_PROXY environment variable
+                url = nil,
+            },
+        },
+    },
     enabled = function()
         return vim.bo.buftype ~= 'nofile' -- when renaming using fzf-lua I don't want completion
             and vim.bo.buftype ~= 'prompt'
