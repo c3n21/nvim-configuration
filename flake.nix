b777740ca5bd576029c22bdc79d974fb67d5823d
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-10-11 17:55:34 +0200

    refactor: include neovim nightly overlay

--- flake.nix
+++ flake.nix
@@ -98,6 +98,10 @@
           # (utils.fixSystemizedOverlay inputs.codeium.overlays
           #   (system: inputs.codeium.overlays.${system}.default)
           # )
+
+          # This allows me in each packageDefinition to do `pkgs.neovim-unwrapped`
+          # isolate every package definition.
+          inputs.neovim-nightly-overlay.overlays.default
         ];
 
       # see :help nixCats.flake.outputs.categories
@@ -786,40 +790,7 @@
             };
           };
 
-        neo =
-          { pkgs, name, ... }:
-          {
-            # they contain a settings set defined above
-            # see :help nixCats.flake.outputs.settings
-            settings = {
-              suffix-path = true;
-              suffix-LD = true;
-              wrapRc = false;
-              neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-              autoconfigure = "suffix";
-            };
-            # and a set of categories that you want
-            # (and other information to pass to lua)
-            categories = {
-              # TODO: dotnet is broken on nixpkgs
-              # dotnet = true;
-              ai = true;
-              astro = true;
-              backend = true;
-              clang = true;
-              fzf-lua = true;
-              general = true;
-              gitPlugins = true;
-              go = true;
-              dap = true;
-              json = true;
-              nix = true;
-              nlua = true;
-              node = true;
-              note = false;
-              tailwindcss = true;
-            };
-          };
+        neo = import ./nix/nixCats/neo.nix;
 
         note =
           { pkgs, name, ... }:
