06342015dedf274f8fd0900340a7d2f83c5b7183
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-10-29 09:00:51 +0100

    feat(conform): enable lsp fallback

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -32,8 +32,7 @@ local config = {
     format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
-        -- disable for this reason: https://github.com/neovim/neovim/issues/26520
-        lsp_fallback = false,
+        lsp_fallback = true,
     },
     -- Set the log level. Use `:ConformInfo` to see the location of the log file.
     log_level = vim.log.levels.DEBUG,
